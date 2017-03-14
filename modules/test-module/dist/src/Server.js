9d19788daf8a58d9cc41582346930414770455b5
qwe qwe <efor@ngs.ru>
2017-03-14 07:08:38 +0000

    temp

---
+++ modules/test-module/dist/src/Server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Server {
+    constructor() {
+        console.log('Тестовый класс создан.');
+    }
+}
+exports.default = Server;
