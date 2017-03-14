b68cfd444342bd4371660447991ea1a7b27747f7
qwe qwe <efor@ngs.ru>
2017-03-14 07:07:28 +0000

    rm

--- modules/test-module/dist/src/Middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Middleware {
-    get middleware() {
-        throw new Error('Не определен middleware.');
-    }
-}
-exports.default = Middleware;
